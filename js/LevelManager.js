e74b5035a74b0c616c32211e1c7e1a906586720c
ronb12 <ronellbradley@gmail.com>
2025-07-15 09:12:54 -0400

    Rename and add LevelManager.js with correct case for deployment

---
+++ js/LevelManager.js
@@ -0,0 +1,10 @@
+class LevelManager {
+    constructor(canvas, ctx) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.level = 1;
+    }
+    update(dt) {}
+    draw(ctx) {}
+}
+window.LevelManager = LevelManager; 
