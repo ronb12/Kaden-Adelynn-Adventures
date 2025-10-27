eef4ad42e341c728cb8743b26bcdd6cde7e44f75
ronb12 <ronellbradley@gmail.com>
2025-10-27 11:57:54 -0400

    Fix service worker asset caching for PWA

---
+++ public/sw.js
@@ -0,0 +1,61 @@
+// Service Worker for PWA
+const CACHE_NAME = 'kaden-adventures-v1'
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.svg',
+  '/icon-192.png',
+  '/icon-512.png'
+]
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Service Worker: Cache opened')
+        return cache.addAll(urlsToCache)
+      })
+  )
+})
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Removing old cache', cacheName)
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+})
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        if (response) {
+          return response
+        }
+        return fetch(event.request).then((response) => {
+          // Cache dynamic assets
+          if (response.status === 200) {
+            const responseToCache = response.clone()
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, responseToCache)
+            })
+          }
+          return response
+        })
+      })
+  )
+})
+
